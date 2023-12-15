57a718cf21f0298de7416437c42e826cb257c894
Abdoulaye Coulibaly <abdoullahkcoulibaly1@gmail.com>
2023-12-15 17:10:06 +0000

    Initial commit

---
+++ lib/app_widget.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './config_reader.dart';
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Provider.of<Color>(context)),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter CI with Codemagic'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+
+     _counter += ConfigReader.getIncrementAmount();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+ 
+    return Scaffold(
+      appBar: AppBar(
+       
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      
+        title: Text(widget.title),
+      ),
+      body: Center(
+      
+        child: Column(
+        
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+             Text(
+              'Revealed secret:\n${ConfigReader.getSecretKey()}',
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ lib/main_dev.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_codemagic/environment.dart';
+import 'package:flutter_codemagic/main_cammon.dart';
+
+Future<void> main() async {
+  await mainCommon(Environment.dev);
+}
